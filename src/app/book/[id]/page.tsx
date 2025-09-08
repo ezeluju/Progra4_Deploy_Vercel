999552eef9631debc04b1744edeed8ed0a27e4d3
Ezequiel Zunino <Ezezuni12@gmail.com>
2025-09-08 01:13:37 -0300

    aver

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -5,9 +5,15 @@ import FavoriteButton from '@/components/FavoriteButton'
 
 async function getBaseUrl() {
   const h = await headers()
-  const host = h.get('host') || 'localhost:3000'
-  const proto = h.get('x-forwarded-proto') || 'http'
-  return `${proto}://${host}`
+  const host = h.get('host')
+  if (host) {
+    const proto = h.get('x-forwarded-proto') || 'http'
+    return `${proto}://${host}`
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+  return process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'
 }
 
 async function getBook(id: string) {
